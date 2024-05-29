2e0ab17a49af5a6cf587fedc3aad0f37f3f6e9d3
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2024-05-29 13:29:53 +0300

    Initialize docker and vite for the app

---
+++ frontend/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    mainFields: [],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
+})
